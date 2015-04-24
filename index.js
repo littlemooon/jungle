752f9c1d92021476251c8c95e9c25366553ea0a4
littlemooon <fredwright0@gmail.com>
2015-04-24 16:32:00 +0200

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Mixin from 'mowgli';
+import RootMixin from 'mowgli';
+import Actions from 'kaa';
+import helpers from './src/helpers';
+
+export default {
+	Mixin,
+	RootMixin,
+	Actions,
+	helpers
+};
