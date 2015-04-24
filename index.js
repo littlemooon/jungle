77d5e370450127b0b7de11b11e8f386fe13998fc
littlemooon <fredwright0@gmail.com>
2015-04-24 16:47:46 +0200

    remove helpers into common package

--- index.js
+++ index.js
@@ -3,11 +3,9 @@
 import Mixin from 'mowgli';
 import RootMixin from 'mowgli';
 import Actions from 'kaa';
-import helpers from './src/helpers';
 
 export default {
 	Mixin,
 	RootMixin,
-	Actions,
-	helpers
+	Actions
 };
