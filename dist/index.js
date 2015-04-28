41d95e2def4a69f42f9bdc48b8ca1f81458a82d4
littlemooon <fredwright0@gmail.com>
2015-04-28 16:07:10 +0200

    build

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var _interopRequireDefault = function (obj) { return obj && obj.__esModule ? obj : { 'default': obj }; };
+
+Object.defineProperty(exports, '__esModule', {
+	value: true
+});
+
+var _Mixin = require('mowgli');
+
+var _Mixin2 = _interopRequireDefault(_Mixin);
+
+var _RootMixin = _interopRequireDefault(_Mixin);
+
+var _Actions = require('kaa');
+
+var _Actions2 = _interopRequireDefault(_Actions);
+
+'use strict';
+
+exports['default'] = {
+	Mixin: _Mixin2['default'],
+	RootMixin: _RootMixin['default'],
+	Actions: _Actions2['default']
+};
+module.exports = exports['default'];
